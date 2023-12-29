164606592d1e0ac3d2b622be5d9248f16d9e1471
“SudiptaSaha123” <sudiptasaha883@gmail.com>
2023-12-29 23:09:48 +0530

    contact section updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,11 +15,11 @@ const Contact = () => {
     initial='hidden'
     whileInView='show' className='bg-black py-4 sm:py-6 mt-1 sm:mt-20 px-10'>
         <div className='flex flex-col items-start gap-4 sm:gap-10'>
-            <div className='text-white text-[1rem] sm:text-[2rem]  ml-4 flex items-center gap-6'>
+            <div className='text-white text-[1rem] sm:text-[1.5rem]  ml-4 flex items-center gap-6'>
                 <IoMdMail />
                 sudiptasaha883@gmail.com
             </div>
-            <div className='text-white text-[1rem] sm:text-[2rem] ml-4 flex items-center gap-6'>
+            <div className='text-white text-[1rem] sm:text-[1.5rem] ml-4 flex items-center gap-6'>
                 <MdLocalPhone />
                 +91 6290568502
             </div>
